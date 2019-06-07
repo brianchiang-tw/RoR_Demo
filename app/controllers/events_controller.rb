7cab58b860d89b96972e696e324046a1f622b170
Brian Chiang <brianchiang1988@icloud.com>
2019-06-07 12:07:33 +0800

    CreateNewEntry

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,37 @@
+class EventsController < ApplicationController
+
+    def index
+        # Get all data from table Event
+        @events = Event.all
+    end
+
+
+    # add a new item in table
+    def new
+        @event = Event.new
+    end
+
+
+
+    # create a new entry in table, data comes from new action
+    def create
+        @event = Event.new( params[:event] )
+        @event.save
+
+        # Redirect to action index after entry creation
+        redirect_to :action => :index
+
+    end
+
+
+
+
+    private
+
+    # Variable check (String parameters mechanism)
+    def event_params
+        params.require(:event).permit(:name, :description)
+    end
+
+ 
+end
