64d9092ef5aa0349c00675037d79cf67a182eade
Brian Chiang <brianchiang1988@icloud.com>
2019-06-07 11:46:01 +0800

    WildCard_Route_Setting

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,14 @@ Rails.application.routes.draw do
 	# Specify homepage as Action "index" of Controller "welcome"
 	# the location of homepage is also defined as "root_path" in RoR
 	root :to => "welcome#index"
+
+	# Set wildcard route
+	# maps URL http://localhost:3000/a/b to controller "a" and action "b"
+	# maps URL http://localhost:3000/a/ to index action
+	match ':controller(/:action(/:id(.:format)))', :via  => :all
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
